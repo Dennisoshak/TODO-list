dd2513da647ea0ed4260e629646abae3d5e6c6f6
dennisoshak <dennisoshak@gmail.com>
2023-05-22 14:14:43 +0300

    edit todos

--- app/index.js
+++ app/index.js
@@ -5,6 +5,8 @@ import './globals.css'
 const TodoApp = () => {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
+  const [onEdit, setOnEdit] = useState(0)
+  const [edited, setEdited] = useState('')
 
 const addTodo = (e) => {
   e.preventDefault();
@@ -20,6 +22,13 @@ const markTodo = (id) => {
     todos.map((todo) => (todo.id === id ? { ...todo, done: !todo.done } : todo))
   );
 };
+const editTodo = (e,i) => {
+ e.preventDefault()
+ const temp = todos
+ temp[i].text = edited
+ setTodos(temp)
+setOnEdit(0)
+}
 return (
   <div className="container">
     <h1>Todo App</h1>
@@ -37,12 +46,31 @@ return (
       variant="outlined" type="submit">Add Todo</Button>
     </form>
     <ul>
-      {todos.map((todo) => (
-        <li key={todo.id} className={`todo-item ${todo.done ? "done" : ""}`}>
+      {todos.map((todo,i) => (
+        
+        <li key={todo.id} draggable className={`todo-item ${todo.done ? "done" : ""}`}>
+          {todo.id===onEdit? (
+           <form className="edit-form" onSubmit={(e)=>editTodo(e,i)}style={{width:"100%"}}>
+           <TextField
+           fullWidth
+           id="fullwidth"
+             type="text"
+             value={edited}
+             onChange={(e) => setEdited(e.target.value)}
+             placeholder={todo.text}
+             sx={{"& fieldset": { border: 'none' },width:"80%",padding:"0"}}
+           /><button type="submit"className="done">Done</button></form>
+          ):(
+            <div className="edit-form">
           <span onClick={() => markTodo(todo.id)}>{todo.text}</span>
+          <div>
+          <button className="edit"disabled={todo.done} onClick={() => setOnEdit(todo.id)}>
+            Edit
+          </button>
           <button className="delete" onClick={() => deleteTodo(todo.id)}>
             Delete
           </button>
+          </div></div>)}
         </li>
       ))}
     </ul>
