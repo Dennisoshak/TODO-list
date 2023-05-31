4c4947f10dc8d09b7aada75d2314e45702feae18
dennisoshak <dennisoshak@gmail.com>
2023-05-31 14:47:54 +0300

    gb pages

--- next.config.js
+++ next.config.js
@@ -2,3 +2,13 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+
+module.exports = {
+    // Other configuration options...
+    output: {
+      // Set the output to "export" for static export
+      // You can also add other configuration options here if needed
+      export: true,
+    },
+  };
